0edbd94307a3dda6c82db70efefe48d24ba6c753
guilherme <guilherme00pereira@gmail.com>
2023-11-26 22:09:41 -0300

    first commit

---
+++ generators/root_files.go
@@ -0,0 +1,15 @@
+package generators
+
+type PluginBaseFile struct {
+	Name        string
+	Url         string
+	Description string
+	Author      string
+	AuthorUrl   string
+	Version     string
+	Namespace   string
+}
+
+func CreateRootFiles(p PluginBaseFile) {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module wp28/wp_plugin_gen
+
+go 1.21.1
